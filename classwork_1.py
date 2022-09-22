cd36b35a815bfc01bed3802ee910e329554674aa
ulnkz <ukalina004@gmail.com>
2022-09-22 19:02:52 +0600

    my 1 month projects

---
+++ classwork_1.py
@@ -0,0 +1,72 @@
+"""Встроенные функции, Переменные, Комментарии, Базовые типы данных (строки, числа), Операции над ними"""
+
+# string = "Alina"
+# print(string[0])
+
+# num = input('введите число: ')
+# print(num)
+# print(type(num))
+
+# sum_group = [18, 31, 27, 12, 40, 32, 21, 34, 23]
+# print(sum(sum_group)/len(sum_group))
+
+# print(sum_group)
+
+# average = sum_group / 9
+
+# changed_average = round(average, 2)
+# changed_average1 = '%.2f' % average
+
+# print(type(changed_average))
+# print(type(changed_average1))
+
+# print('средний возраст группы', round(average, 2))
+# print('средний возраст группы', '%.2f' % average)
+
+# a = '5'
+# b = '2'
+# print(float(a)+int(b))
+# c = 10
+# print(str(c)+ 'abc')
+
+# a = 5
+# b = 10
+# c = 2.5
+# b = c + b
+# print(b)
+# print(type(b))
+
+# c = a+b
+
+# print(b+a)
+# print(b-a)
+# print(b*a)
+# print(b/a)
+# print(b//a)
+# print(b**4)
+# print(b%a)
+# print(20%3)
+# print(20//3)
+
+# number = 1
+# num_one = 1
+# numberTwo = 1
+# Number_third = 1
+# number4 = 1
+
+# name = 'jack'
+# name1 = input('What is your name? ')
+# surname = "neil conor"
+
+# age = 20
+# job = 'mvd'
+
+# print(surname.capitalize())
+# print(f"Hello, {name.title()} {surname}, works in {job.upper()}")
+
+# print(type(name))
+# print(type(age))
+
+# print("Hello ", name, surname)
+# print("Hello " + name + surname)
+# print("Hello, {}".format(name + surname))
